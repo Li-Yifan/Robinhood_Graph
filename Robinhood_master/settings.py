0d3c19027a3058d9e36d4c812c311dd6c7bedea0
Yifan Li <33741973+Li-Yifan@users.noreply.github.com>
2021-08-15 23:16:44 +0800

    try to add static to heroku

--- Robinhood_master/settings.py
+++ Robinhood_master/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f@6p7qogsvy-s8-vdj2$ut87h^f*lyu%053=&$l(n7$$=b-mh$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic', # new
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig', # for new app -- pages
 ]
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,8 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'), # gives absolute path
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile'), # gives absolute path
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
